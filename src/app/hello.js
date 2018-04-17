02f89759cec91b4d7d2036dbd587080625d0e8e1
Nandeesh Shankarappa <nandeesh.s@hcl.com>
2018-04-17 14:26:02 +0530

    First commit

---
+++ src/app/hello.js
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'fountain-app',
+  template: require('./hello.html')
+})
+export class HelloComponent {
+  constructor() {
+    this.hello = 'Hello World!';
+  }
+}
